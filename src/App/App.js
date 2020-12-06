b085fe6f2b75588bc455ee057a06a128d4946597
Jonathon Joyner <jonathonandon@gmail.com>
2020-12-06 14:41:08 -0600

    added firebase auth

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,48 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import fbConnection from '../Helpers/Data/connection';
 import './App.scss';
+import Auth from '../Components/Auth';
+import OutfitContainer from '../Components/OutfitContainer';
+import MyNavbar from '../Components/MyNavbar';
+
+fbConnection();
 
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+    const loadComponent = () => {
+      let component = '';
+      if (authed) {
+        component = <OutfitContainer />;
+      } else {
+        component = <Auth />;
+      }
+      return component;
+    };
+
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">I am a button</button>
+        <MyNavbar authed={authed} />
+        {loadComponent()}
       </div>
     );
   }
---
+++ src/Components/OutfitContainer/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class OutfitContainer extends Component {
+  state = { }
+
+  render() {
+    return (
+      <div className='Outfits-Container'>
+        <h2>outfits</h2>
+      </div>
+    );
+  }
+}
+
+export default OutfitContainer;
